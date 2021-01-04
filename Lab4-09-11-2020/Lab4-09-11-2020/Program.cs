039bc81aaae9cfbda0c025294ffc9e42a9b3cccf
Asanali Bulatov <w62890@student.wsiz.edu.pl>
2021-01-04 18:13:22 +0600

    Lab4-09-11-2020 and lab_8_07_12_2020, fix naming

---
+++ Lab4-09-11-2020/Lab4-09-11-2020/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+
+namespace Lab4_09_11_2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ex3();
+        }
+
+        public static double Median(int[] arr)
+        {
+            Array.Sort(arr);
+
+            if (arr.Length % 2 == 0)
+            {
+                return (arr[ (arr.Length / 2) - 1] + arr[(arr.Length / 2)]) / 2.0;
+            }
+            else
+            {
+                return arr[arr.Length / 2];
+            }
+        }
+
+        public static int Mode(int[] arr)
+        {
+            return  arr.GroupBy(v => v)
+                .OrderByDescending(g => g.Count())
+                .First()
+                .Key;
+        }
+
+        public static double StdDev(int[] arr)
+        {
+            double avg = arr.Average();
+            return Math.Sqrt(arr.Average(v => Math.Pow(v - avg, 2)));
+        }
+
+        public static void Ex1()
+        {
+            Console.WriteLine("Enter the size of array");
+            var N = Convert.ToInt32(Console.ReadLine());
+
+            int[] arr = new int[N];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine("Enter the number");
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Array.Sort(arr);
+
+            Console.WriteLine($"Mean is {arr.Average()}, median is { Median(arr)}, mode is { Mode(arr)}, stdDev is { StdDev(arr)}");
+
+        }
+
+        public static void Ex2()
+        {
+            Console.WriteLine("Enter the size of array");
+            var N = Convert.ToInt32(Console.ReadLine());
+
+            int[] arr = new int[N];
+
+            for (int i = N - 1; i >= 0; i--)
+            {
+                Console.WriteLine("Enter the number");
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write($"{arr[i]} ");
+            }
+
+
+
+        }
+
+        public static void Ex3()
+        {
+            Console.WriteLine("Enter the size of array");
+            var N = Convert.ToInt32(Console.ReadLine());
+
+            int[] arr = new int[N];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine("Enter the number");
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            var dict = new Dictionary<int,int>();
+
+            foreach (int a in arr)
+            {
+                if (dict.ContainsKey(a))
+                    dict[a] = dict[a] + 1; 
+                else
+                    dict[a] = 1;
+            }
+
+            foreach (KeyValuePair<int, int> kvp in dict)
+            {
+
+                if (kvp.Value > 1)
+                {
+                    Console.WriteLine($"Duplicate of {kvp.Key}, {kvp.Value} times");   
+                }
+
+            }
+
+            //dict.Select(i => $"{i.Key}: {i.Value}").ToList().ForEach(Console.WriteLine);
+
+
+        }
+
+        public static void Ex4()
+        {
+            int[,] array1 = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+            int[,] array2 = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+            int[,] array3 = new int[3,3];
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    
+                }
+            }
+
+        }
+
+
+
+    }
+}
---
+++ lab_8_07_12_2020/lab_8_07_12_2020/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace lab_8_07_12_2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var test1 = new Test(20);
+            Console.WriteLine();
+            var test2 = new Test();
+            Console.WriteLine();
+            // 2 + 4 + 6 + 8 + 10 = 30
+            var SumAp = new SumAp(2, 2, 5);
+            Console.WriteLine();
+            var Floyd = new Floyd(10);
+        }
+
+        class SumAp
+        {
+            public SumAp(int first, int differ, int n) => Console.WriteLine($"Sum is {(2 * first + differ * (n - 1)) * n / 2}");
+        }
+
+
+
+        class Floyd
+        {
+            public Floyd(int n)
+            {
+                // 1
+                // 1 2
+                // 3 4 5
+                var v = 1;
+                for (int i = 1; i <= n; i++)
+                {
+                    for (int j = 1; j <= i; j++)
+                    {
+                        Console.Write($"{v} ");
+                        v++;
+                    }
+
+                    Console.WriteLine();
+                }
+            }
+        }
+
+
+        class Test
+        {
+
+            public Test(int n)
+            {
+                var sum = 0;
+                for (int i = 1; i < n; i += 2)
+                {
+                    sum += i;
+                }
+
+                Console.WriteLine($"Sum is {sum}");
+            }
+
+            public Test()
+            {
+                var height = 10;
+
+                for (int i = height - 1; i >= 0; --i)
+                {
+                    for (int j = 0; j < i; ++j)
+                    {
+                        Console.Write(" ");
+                    }
+
+                    for (int j = 0; j < (height - i) * 2 - 1; ++j)
+                    {
+                        Console.Write("*");
+                    }
+
+                    Console.WriteLine();
+                }
+            }
+
+
+        }
+    }
+}
