d59db4ea8ba026f279347fdee6a087b94fcc5f50
Asanali Bulatov <w62890@student.wsiz.edu.pl>
2021-02-04 17:06:27 +0600

    Lab 14 done

---
+++ Lab14-01-02-2021/Calculator/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.TextLength == 0 || textBox2.TextLength == 0)
+            {
+                MessageBox.Show("ENTER THE NUMBERS");
+                return;
+            }
+
+
+            textBox3.Text = $"{Convert.ToInt32(textBox1.Text) + Convert.ToInt32(textBox2.Text)}";
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.TextLength == 0 || textBox2.TextLength == 0)
+            {
+                MessageBox.Show("ENTER THE NUMBERS");
+                return;
+            }
+
+            textBox3.Text = $"{Convert.ToInt32(textBox1.Text) - Convert.ToInt32(textBox2.Text)}";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (textBox1.TextLength == 0 || textBox2.TextLength == 0)
+            {
+                MessageBox.Show("ENTER THE NUMBERS");
+                return;
+            }
+
+            textBox3.Text = $"{Convert.ToInt32(textBox1.Text) * Convert.ToInt32(textBox2.Text)}";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (textBox1.TextLength == 0 || textBox2.TextLength == 0)
+            {
+                MessageBox.Show("ENTER THE NUMBERS");
+                return;
+            }
+
+            textBox3.Text = $"{Convert.ToInt32(textBox1.Text) / Convert.ToInt32(textBox2.Text)}";
+        }
+    }
+}
---
+++ Lab14-01-02-2021/PrimeNum/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PrimeNum
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public static bool IsPrime(int number)
+        {
+            if (number <= 1) return false;
+            if (number == 2) return true;
+            if (number % 2 == 0) return false;
+
+            var boundary = (int)Math.Floor(Math.Sqrt(number));
+
+            for (int i = 3; i <= boundary; i += 2)
+                if (number % i == 0)
+                    return false;
+
+            return true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (IsPrime(Convert.ToInt32(textBox1.Text)))
+            {
+                MessageBox.Show("PRIME");
+            }
+            else
+            {
+                MessageBox.Show("NOT PRIME");
+            }
+        }
+    }
+}
